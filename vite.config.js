c8092e61b5f98f2b272af39b95078770b6e42025
Yuval Greenfield <ubershmekel@gmail.com>
2021-04-23 22:57:13 -0700

    Fix github embedded deployment

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,6 @@
 export default {
+  // Define `base` because this deploys to user.github.io/repo-name/
+  base: './',
   build: {
     // Do not inline images and assets to avoid the phaser error
     // "Local data URIs are not supported"
